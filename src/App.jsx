8a430a6ecc5f738e5e810c8c7e2be9975d01993c
Rajat Sharma <rajatsharma221098@gmail.com>
2026-03-09 18:53:35 +0530

    component called

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import Cart from "./components/Cart";
 import { CartContextProvider } from "./store/CartContext";
+import { UserProgressContextProvider } from "./store/UserProgressContext";
+import CheckOut from "./components/Checkout";
 
 function App() {
   return (
-    <CartContextProvider>
-      <Header />
-      <Meals />
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+        <CheckOut />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
